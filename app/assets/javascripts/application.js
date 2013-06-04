f66e43861a14445e8e2961ef3f69b45a8b564e98
Dan Park <dpinch86@gmail.com>
2013-06-04 15:17:32 -0500

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,13 @@
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+
+// $(document).ready(function() {
+//   $("li").on("click", function(e) {
+//     $(e.target).css("color", "red");
+//   });
+// });
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Notebook::Application.routes.draw do
+
+  resources :notes
+
+
+  root to: "notes#index"
+
+  get '/mockup' => 'pages#mockup'
+
+
+end
